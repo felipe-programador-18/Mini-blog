b9d5e2b8d570421212974d2d56137be60b3c2529
Felipe <machadofelipe2016@outlook.com>
2022-07-04 17:06:59 -0300

    c7-adding routes inside principles page

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/about';
+import Home from './Pages/Home/home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Mini Blog</h1>
+     <BrowserRouter>
+     <div className='container' >
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route path='/about'  element={<About/>} /> 
+
+      </Routes>
+     </div>
+     </BrowserRouter>
     </div>
   );
 }
