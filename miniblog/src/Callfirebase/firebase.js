a5b68a1bb3e7dac1a50ddbdbbf614ed0989a2f61
Felipe <machadofelipe2016@outlook.com>
2022-07-04 16:02:21 -0300

    c1-create folder to manage firebase

---
+++ miniblog/src/Callfirebase/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase'
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbc7EyCJPQtrKlxy9r2h3PySoIfmoA3WU",
+  authDomain: "mini-blog-a74af.firebaseapp.com",
+  projectId: "mini-blog-a74af",
+  storageBucket: "mini-blog-a74af.appspot.com",
+  messagingSenderId: "463884436374",
+  appId: "1:463884436374:web:8c5380258d2672c8a4d3b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+export {db}
