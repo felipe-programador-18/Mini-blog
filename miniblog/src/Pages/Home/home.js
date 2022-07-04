e1f85abe5aa046cf990775e60ad825d686ec989b
Felipe <machadofelipe2016@outlook.com>
2022-07-04 17:06:30 -0300

    c6-adding page and create about and home

---
+++ miniblog/src/Pages/Home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from  "./home.module.css"
+
+
+const Home = () => {
+    return(
+    <div>
+    
+        <h1>Home</h1>
+    
+    </div>
+    )
+}
+
+export default Home
