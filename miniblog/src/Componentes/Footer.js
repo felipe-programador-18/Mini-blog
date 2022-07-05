7466ecf4d8f7fd778a86e618aab03193df147e90
Felipe <machadofelipe2016@outlook.com>
2022-07-05 14:40:00 -0300

    c11- gave stules in the project in file footer

--- miniblog/src/Componentes/Footer.js
+++ miniblog/src/Componentes/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './footer.module.css'
 const Footer = () => {
     return (  
-        <footer>
+        <footer className={styles.footer} >
             <h3>Escreva sobre o que você tem interesse </h3>
             <p>Mini blog. Criado por: </p>
         </footer>
