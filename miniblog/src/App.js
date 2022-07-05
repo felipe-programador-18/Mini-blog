0084b2297ad100dacbbb74bddb82044d17dd1f19
Felipe <machadofelipe2016@outlook.com>
2022-07-05 15:48:02 -0300

    c19-adding router to login and register

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Footer from './Componentes/Footer';
 import Navbar from './Componentes/Navbar';
+import Login from './Login/login';
 import About from './Pages/About/about';
 import Home from './Pages/Home/home';
+import Register from './Register/Register';
 
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}  />
           <Route path='/about'  element={<About/>} /> 
+          <Route path='/register' element={ <Register/> }  />
+          <Route path='/login'  element={ <Login/> } /> 
 
         </Routes>
       </div>
