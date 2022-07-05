960d4eef07d64dfc66c7ad3f22e92282011d2607
Felipe <machadofelipe2016@outlook.com>
2022-07-05 15:47:24 -0300

    c18- adding more two navlink about navbar

--- miniblog/src/Componentes/Navbar.js
+++ miniblog/src/Componentes/Navbar.js
@@ -9,14 +9,25 @@ const Navbar = () => {
       Mini <span>Blog</span>   
      </NavLink> 
       
+      
       <ul className={styles.links_list} >
         <li>
          <NavLink to='/' className={({isActive}) => (isActive ? styles.active : '' ) } > Home</NavLink>     
+        </li>
+        
+        <li>
+         <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '' ) } > Entrar</NavLink>     
+        </li>
+        <li>
+         <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '' ) } > Registrar</NavLink>     
         </li>
         <li>
          <NavLink to='/about'  className={({isActive}) => (isActive ? styles.active : '') }  > Sobre </NavLink>
         </li>
       </ul>
+    
+    
+    
     
     </nav>
   
