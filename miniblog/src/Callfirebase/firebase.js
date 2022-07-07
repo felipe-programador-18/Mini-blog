f55a6915bdfc794943fea5e93437ce5229f676a7
Felipe <machadofelipe2016@outlook.com>
2022-07-07 08:48:50 -0300

    c27-verify issues about connect firebase

--- miniblog/src/Callfirebase/firebase.js
+++ miniblog/src/Callfirebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firebase'
+import {getFirestore} from 'firebase/firestore'
 
 
 // Your web app's Firebase configuration
