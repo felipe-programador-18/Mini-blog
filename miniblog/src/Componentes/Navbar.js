0be7bc5b82b9e74c0a556b89126fd364091e54c2
Felipe <machadofelipe2016@outlook.com>
2022-07-07 15:04:20 -0300

    c39- caught props about file hoock and adding about button

--- miniblog/src/Componentes/Navbar.js
+++ miniblog/src/Componentes/Navbar.js
@@ -8,7 +8,9 @@ const Navbar = () => {
   //caught user about create providers
   const {user} = useAuthValue()
   console.log('verify here' , user)
-
+  const {logout}  = useAutentication()
+  
+  
   return (<div>
     <nav className={styles.navbar} >
      <NavLink to='/' className={styles.brand} >
@@ -20,23 +22,33 @@ const Navbar = () => {
         <li>
          <NavLink to='/' className={({isActive}) => (isActive ? styles.active : '' ) } > Home</NavLink>     
         </li>
-        
+         
+         {/* here i can very that user not stay loggin in the page */}
          {!user && (<>
           <li>
-            <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '' ) } > Entrar</NavLink>     
+            <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '' ) } >
+              Entrar
+            </NavLink>     
           </li>
+          
           <li>
-            <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '' ) } > Registrar</NavLink>     
+            <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '' ) } >
+               Registrar
+            </NavLink>     
           </li>
            
          </>) }
           
         {user && ( <>
           <li>
-          <NavLink to='/posts/create' className={({isActive}) => (isActive ? styles.active : '' ) } >Novo Post</NavLink>     
+          <NavLink to='/posts/create' className={({isActive}) => (isActive ? styles.active : '')}>
+            Novo Post
+          </NavLink>     
         </li>
         <li>
-          <NavLink to='/dashboard' className={({isActive}) => (isActive ? styles.active : '' ) } > Dashboard</NavLink>     
+          <NavLink to='/dashboard' className={({isActive}) => (isActive ? styles.active : '' )}>
+           Dashboard
+          </NavLink>     
         </li>
 
          </>)}  
@@ -44,11 +56,16 @@ const Navbar = () => {
         <li>
          <NavLink to='/about'  className={({isActive}) => (isActive ? styles.active : '') }  > Sobre </NavLink>
         </li>
+        
+        {/* if user it's logount he can logout */}
+        {user && (
+          <li>
+           <button onClick={logout}   >Sair</button>    
+          </li>
+         )}
+      
+    
       </ul>
-    
-    
-    
-    
     </nav>
   
 </div>
