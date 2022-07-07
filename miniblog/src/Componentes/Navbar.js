e3425590ae1cf1d6d12e30dad9487d7381eb5ff2
Felipe <machadofelipe2016@outlook.com>
2022-07-07 10:15:03 -0300

    c31-adding personalite hoock and adding  context in the part applicatoin

--- miniblog/src/Componentes/Navbar.js
+++ miniblog/src/Componentes/Navbar.js
@@ -1,7 +1,13 @@
 import styles from './nav.module.css'
 import { NavLink } from 'react-router-dom'
 
+import { useAutentication } from '../hoock/useAuthentication'
+import { useAuthValue } from '../context/Authcontext'
+
 const Navbar = () => {
+  //caught user about create providers
+  const {user} = useAuthValue()
+  console.log('verify here' , user)
 
   return (<div>
     <nav className={styles.navbar} >
@@ -15,12 +21,26 @@ const Navbar = () => {
          <NavLink to='/' className={({isActive}) => (isActive ? styles.active : '' ) } > Home</NavLink>     
         </li>
         
-        <li>
-         <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '' ) } > Entrar</NavLink>     
+         {!user && (<>
+          <li>
+            <NavLink to='/login' className={({isActive}) => (isActive ? styles.active : '' ) } > Entrar</NavLink>     
+          </li>
+          <li>
+            <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '' ) } > Registrar</NavLink>     
+          </li>
+           
+         </>) }
+          
+        {user && ( <>
+          <li>
+          <NavLink to='/posts/create' className={({isActive}) => (isActive ? styles.active : '' ) } >Novo Post</NavLink>     
         </li>
         <li>
-         <NavLink to='/register' className={({isActive}) => (isActive ? styles.active : '' ) } > Registrar</NavLink>     
+          <NavLink to='/dashboard' className={({isActive}) => (isActive ? styles.active : '' ) } > Dashboard</NavLink>     
         </li>
+
+         </>)}  
+            
         <li>
          <NavLink to='/about'  className={({isActive}) => (isActive ? styles.active : '') }  > Sobre </NavLink>
         </li>
