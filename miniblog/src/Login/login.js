c8033fac1f4812b4daea42748f19b0216faeef33
Felipe <machadofelipe2016@outlook.com>
2022-07-07 15:01:01 -0300

    c37-create all structure about login

--- miniblog/src/Login/login.js
+++ miniblog/src/Login/login.js
@@ -1,7 +1,79 @@
-import './login.module.css'
+import  styles from './login.module.css'
+
+import React,{useState, useEffect} from 'react'
+import { useAutentication } from '../hoock/useAuthentication'
+
+
 const Login = () => {
-    return ( <div>
-        <h2>Login</h2>
+ const [email, setEmail] = useState("")
+ const [password, setPassword] = useState("")
+ const [error, setError] = useState("")
+  
+ // this properties is destructment about my persolaties hoock!
+ const {createUser,error:authError, loading} = useAutentication()
+ 
+ // this care about send date!
+ const handlingSubmit = async (e) => {
+    e.preventDefault()
+    setError("")
+    const user = {
+        email,
+        password,
+    }
+   
+    const res = await  createUser(user)
+    
+    console.log(user)
+
+ }
+ //need verify about my setError change!!  
+  useEffect(() => {
+     if(authError){
+        setError(authError)
+     }
+  },[authError])
+
+    
+    
+    return ( <div className={styles.login} >
+        <h1>Entrar</h1>
+        <p>Faça o login para poder entrar no sistema.</p>
+
+
+       <form onSubmit={handlingSubmit}>
+  
+        <label>
+         <span>Email:</span>
+         <input type='email' 
+         name='email' 
+         required 
+         placeholder='E-mail do Usuário'  
+         value={email}     
+         onChange={(e)=>   setEmail(e.target.value)}  />
+        </label>
+
+        <label>
+         <span>Senha:</span>
+           <input type='password'
+           name='password' 
+           required 
+           placeholder='Insira sua Senha'  
+           value={password} 
+           onChange={(e) => setPassword(e.target.value)} />
+        </label>
+
+    
+           {!loading && <button className='btn' >Entrar</button> }
+           
+           {loading && (<button className='btn' disabled >
+            Aguarde .... 
+           </button>) }
+
+          {error && <p className='error' > {error} </p>}
+
+
+</form>
+
     </div>)
 }
 
