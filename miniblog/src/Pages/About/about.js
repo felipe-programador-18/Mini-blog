89b566455db0816f59e4fd52198afb08c7a7b5d9
Felipe <machadofelipe2016@outlook.com>
2022-07-07 15:02:25 -0300

    c38-gave some parameter and text in the about

--- miniblog/src/Pages/About/about.js
+++ miniblog/src/Pages/About/about.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './about.module.css'
 
 const About  = () => {
-  return (<div>
+  return (<div className={styles.about} >
 
-    <h1 >About!!</h1>
+   <h2>Sobre  o Mini <span>Blog</span>   </h2>
+   <p>Esse projeto é um projeto com React no Front-End e Firebase no Bakc-end. </p>
+   <Link to='/posts/create' className='btn'  >Criar Post</Link>
 
   </div> )
 }
