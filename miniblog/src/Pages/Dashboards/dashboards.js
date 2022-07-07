1ce8ebdd8db15e1d5db1c83d78726b3d86d05bed
Felipe <machadofelipe2016@outlook.com>
2022-07-07 10:13:01 -0300

    c29-create dashboard to adding in navibar

---
+++ miniblog/src/Pages/Dashboards/dashboards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./dashboard.module.css"
+
+const Dashboard = () => {
+    
+    return(<div className={''} >
+        <h1>Dashboard</h1>
+
+    </div>)
+}
+
+export default Dashboard
