a1f10893634e842e3adcadb87b8e92cd60fb5c20
Felipe <machadofelipe2016@outlook.com>
2022-07-07 08:46:53 -0300

    c25-create all context

---
+++ miniblog/src/context/Authcontext.js
@@ -0,0 +1,14 @@
+import React, {useContext, createContext} from 'react'
+
+export const AuthContext = createContext()
+
+// this way interest and different about create context!!
+export const AuthProvider = ({children, value}) => {
+ return <AuthContext.Provider value={value} >
+       {children}
+ </AuthContext.Provider>
+}
+
+export const useAuthValue = () =>{
+    return useContext(AuthContext)
+}
