b2bc09d1e30062ace8cbc50d9aeda2f6b996ee1f
Felipe <machadofelipe2016@outlook.com>
2022-07-08 19:31:09 -0300

    c45-adding p

--- miniblog/src/Pages/About/about.js
+++ miniblog/src/Pages/About/about.js
@@ -7,6 +7,7 @@ const About  = () => {
 
    <h2>Sobre  o Mini <span>Blog</span>   </h2>
    <p>Esse projeto é um projeto com React no Front-End e Firebase no Bakc-end. </p>
+   <h3>This is project together work with react and firebase is lot interesting for users, posted them all idea's.</h3>
    <Link to='/posts/create' className='btn'  >Criar Post</Link>
 
   </div> )
