0c9a21c5afab191c944ff79d874d441148f59665
Felipe <machadofelipe2016@outlook.com>
2022-07-11 10:45:58 -0300

    c48-works with personalites hoock to can make destrucment

--- miniblog/src/Pages/Createpost/createpost.js
+++ miniblog/src/Pages/Createpost/createpost.js
@@ -3,7 +3,7 @@ import styles from  "./create.module.css"
 import { useNavigate } from 'react-router-dom'
 
 import { useAuthValue } from '../../context/Authcontext'
-
+import { useInsertDocument } from '../../hoock/useinsertDocument'
 
 const CreatePost = () => {
    const[title, setTitle] = useState("")
@@ -11,11 +11,34 @@ const CreatePost = () => {
    const[body, setBody] = useState("")
    const[tags, setTags] = useState([]) 
    const[formError, setFormError] = useState("")
-
+    
+   const { insertDocument,response } = useInsertDocument("posts") 
+   const {user} = useAuthValue()
+  
+  
    //const {createUser,error:authError, loading} = useAutentication()
    const handlingSubmit = (e) => {
     e.preventDefault()
+    setFormError("")
+    //validation url picture 
+    //create array of tag
+    // check up all values!!
+      
+   
+     insertDocument({
+    title, 
+    image,
+    body ,
+    tags,
+    uid:user.uid,
+    createdBy: user.displayName
+    })
+   //reminder latter redirect to home page!!
+
+
    }
+
+   
 
    return( <div className={styles.create_post} >
        <h2>Criar post</h2>
@@ -60,14 +83,14 @@ const CreatePost = () => {
             value={tags}
             onChange={(e)=>  setTags(e.target.value)}  />
           </label> 
-          
-         {/* {!loading && <button className='btn' >Cadastrar</button> }
+         
+          {!response.loading && <button className='btn' >Cadastrar</button> }
            
-           {loading && (<button className='btn' disabled >
+           {response.loading && (<button className='btn' disabled >
             Aguarde .... 
            </button>) }
 
-         {error && <p className='error' > {error} </p>} */ }
+         {response.error && <p className='error' > {response.error} </p>}  
 
            
         
