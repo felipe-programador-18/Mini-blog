f98d522c5e33721f037d205af36d39eecad17e98
Felipe <machadofelipe2016@outlook.com>
2022-07-11 14:42:43 -0300

    c52-adding usenavigate and link ann create all strcuture about home and manage new post

--- miniblog/src/Pages/Home/home.js
+++ miniblog/src/Pages/Home/home.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from  "./home.module.css"
+
+import { useNavigate, Link } from 'react-router-dom'
+
+
 
 
 const Home = () => {
-    return(
-    <div>
+    const [query, setQuery] = useState('')
+    const [posts] = useState([])
     
-        <h1>Home</h1>
+    
+    const handSubmit = (e) => {
+        e.preventDefault()
+
+    }
+    
+    return(
+    <div  className={styles.home}>
+      <h1>Veja nossos postes mais recentes.</h1>
+
+      <form onSubmit={handSubmit} className={styles.search_form} >
+        <label >
+            <input type="text" placeholder='ou busque por tags...' onChange={(e) => setQuery(e.target.value)}  />
+        </label>
+        <button className='btn btn-dark' >Pesquisar.</button>
+      
+      </form>
+      
+      
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+            <div className={styles.nopost}>
+            <p> Não foram encontrados posts. </p>
+            
+            <Link to='/posts/create'  className='btn' > Crie seu Primeiro Post. </Link>
+            </div>
+        )  }
+      </div>
     
     </div>
     )
