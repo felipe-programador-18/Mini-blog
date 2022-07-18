958aa247e3c8193c7f31495ed0d2cc1631c97d1e
Felipe <machadofelipe2016@outlook.com>
2022-07-18 12:13:35 -0300

    c72-rearrange and see other thing

--- miniblog/src/hoock/useFecthingdocuments.js
+++ miniblog/src/hoock/useFecthingdocuments.js
@@ -37,14 +37,16 @@ export const useFecthingDocuments =(docCollection,search=null,uid=null) =>{
      
      try {
       let q ;
-       q = await  query(collectionRef)
-             
+      q = await  query(collectionRef)
+      
       if(search){
         q= await query(collectionRef, where("tags",'array-contains', search), orderBy("createdAt","desc") )
+        
       }else{
-        q = await query(collectionRef)
+       // q = await query(collectionRef, orderBy("createdAt", "desc"))
+       q = await query(collectionRef)
       }
-       
+        console.log("here verify about search", search)
       const querySnapshot = await getDocs(q,orderBy("createdAt","desc"));
       
       setDocument(
