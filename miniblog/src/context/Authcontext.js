9f78a891dc5027a2b194e0345b9bbb230329d7c0
Felipe <machadofelipe2016@outlook.com>
2023-01-17 12:50:46 -0300

    c96-to rearrange the code

--- miniblog/src/context/Authcontext.js
+++ miniblog/src/context/Authcontext.js
@@ -4,6 +4,7 @@ export const AuthContext = createContext()
 
 // this way interest and different about create context!!
 export const AuthProvider = ({children, value}) => {
+
  return <AuthContext.Provider value={value} >
        {children}
  </AuthContext.Provider>
